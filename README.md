be2cc0c55bdcea5db1dff13774b49ddcc58f206e
thyngster <thyngster@gmail.com>
2016-11-21 17:01:00 +0100

    Refactoring + New Custom Dimensions
    Full code refactoring and cleaning up
    Added Request Method Dimension
    Added Server Protocol Dimension
    Added the requesting IP address reverse domain root name

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# wp-seo-ga
-Wordpress Plugin to log search bots visits to Google Analytics
+# SEO Meets Google Analytics
+
+[TOC]
+
+What's this?
+-----------------
+
+Is a Wordpress Plugin to log search bots visits to Google Analytics. This plugin used the uap-core library for PHP in order to detect the bots based on their User Agent.
+
+
+Installation
+------------
+Firstly you need to upload a the wp-seo-ga folder to your wp-content/plugins folder.
+After that just activate the plugin, and setup the Property ID where you want to send the data.
+
+This integration does use custom dimensions in order to track a lot of extra bots/requests info. Please refer to Custom Dimensions sections in order to configure your Google Analytics Property.
+
+> **Note:** The custom dimensions setup is not needed at all if you only want to track the pages visited by bots, but we definetly recomended setting up a new property with those custom dimensions in order to track the bot names, the response, codes, etc
+
+Custom Dimensions
+--------------------
+|Custom Dimension|Value|Scope|
+|:-:|:-|:-:|
+|#1|Bot Name|User|
+|#2|Bot Device|User|
+|#3|Bot Model|User|
+|#4|Original User Agent|Hit|
+|#5|Response Status|Hit|
+|#6|Cliend ID|User|
+|#7|User ID|User|
+|#8|Total Memory Used|Hit|
+|#9|Total Processing Time|Hit|
+|#10|Total Number of Queries|Hit|
+|#11|Bot Name|Hit|
+|#12|Bot Device|Hit|
+|#13|Bot Model|Hit|
+|#14|Request Method|Hit|
+|#15|Server Protocol|Hit|
+|#16|Reverse Hostname|User|
+
+# To-Do
+
+ - Add sanity checks before being able to turn on the plugin
+	 - PHP Version
+	 - gethostbyaddr availability
+	 - Check Streams availability for sending hits
+ - Improve the plugin admin ui
+ - Add 500 errors tracking, based on register_shutdown_function (Needs PHP> 5.2)
+ - Add Predefined Report templates to the documentation
+
+# ChangeLog
+
+v0.2
+
+ - Full code refactoring and cleaning up
+ - Added Request Method Dimension
+ - Added Server Protocol Dimension
+ - Added the requesting IP address reverse domain root name
+
+v0.1
+
+ - First Public Release
+
+# Support the plugin
--- wp-seo-ga.php
+++ wp-seo-ga.php
@@ -15,193 +15,199 @@ Copyright 2016  David Vallejo  (email : thyngster@gmail)
 require_once 'vendor/autoload.php';
 use UAParser\Parser;
 
-
 if(!class_exists('WP_Plugin_Seo_Ga'))
 {
-    $user_agent_parsed;
     class WP_Plugin_Seo_Ga
     {
         /**
          * Construct the plugin object
          */
-
         public function __construct($data)
         {
-		$user_agent_parsed = $data;
-		if($user_agent_parsed->device->family=="Spider" ||$_GET["dev"]==1){
-	        	add_action( 'wp', 'track_search_bots' );
-		}
+          // Add Main action, Track when WP has been fully loaded, to know the 404 status.
+          add_action( 'wp', array( $this, 'trackGoogleAnalytics' ) );
+          // Admin panel Actions
+          add_action("admin_menu", "add_theme_menu_item");
+          add_action("admin_init", "display_theme_panel_fields");
 
-		function theme_settings_page()
-		{
-		    ?>
-			    <div class="wrap">
-			    <h1>SEO Meets Google Analytics</h1>
-	        <?php
-		   $active_tab = isset( $_GET[ 'tab' ] ) ? $_GET[ 'tab' ] : 'general_options';
-	        ?>
-		           <h2 class="nav-tab-wrapper">
-			            <a class="nav-tab <?php echo $active_tab == 'general_options' ? 'nav-tab-active' : ''; ?>" href="?page=wp-seo-ga-panel&tab=general_options" class="nav-tab">General</a>
-			            <a class="nav-tab <?php echo $active_tab == 'about' ? 'nav-tab-active' : ''; ?>" href="?page=wp-seo-ga-panel&tab=about" class="nav-tab">About</a>
-		        </h2>
-			    <form method="post" action="options.php">
-			        <?php
-				    if( $active_tab == 'general_options' ) {
-				            settings_fields("section");
+          function add_theme_menu_item()
+          {
+              add_menu_page("WP SEO GA", "WP SEO GA", "manage_options", "wp-seo-ga-panel", "theme_settings_page", null, 99);
+          }
 
-					    echo "<br /><br />This plugin takes care of detecting search bot visits, and register them on Google Analytics using the measurement protocol. Got to the <a href='/wp-admin/admin.php?page=wp-seo-ga-panel&tab=about'>About tab</a> to get more info.";
-				            do_settings_sections("wp-seo-ga-options");
-				            echo "* Please use a value with the following format:<br /> <strong>/(UA|YT|MO)-\d+-\d+/</strong>";
-				            submit_button();
-				    }else{
-				            do_settings_sections("wp-seo-ga-about");
-					?>
-					<div id="post-body-content">
-						WP SEO GA version 0.1
-
-						<hr />
-						<br />
-						<br />This plugin uses <a href="https://github.com/ua-parser/uap-core">uap-core</a> to detect whenever a search bot visits your page, and then send the info about the bot and visited page to the configured Google Analytics Property ID.
-						<br />
-						<br />Read more about the Measurement Protocol <a href="https://developers.google.com/analytics/devguides/collection/protocol/v1/?hl=en">here</a> .
-						<br />
-						<br />Take in mind this a Beta product and it has not been deeply tested. Try it first on a testing enviroment before installing it on a production site.
-						<br />
-						<br />You may find the sourcecode on the following github repo: <a href="https://github.com/thyngster/wp-seo-ga">https://github.com/thyngster/wp-seo-ga</a>
-						<br />You may read some more info and leave a comment on the following blog post: <a href="https://www.thyngster.com/seo-meets-ga-tracking-search-engines-visits-within-measurement-protocol">https://www.thyngster.com/seo-meets-ga-tracking-search-engines-visits-within-measurement-protocol</a>
-						<br />
-						<br /><a href="https://www.twitter.com/thyng">@thyng</a>
-
-&nbsp;
-					</div>
-
-				       <?php
-					    echo "<p>This </p>";
-				    }
-			        ?>
-			    </form>
-			</div>
-		<?php
-		}
-
-		function display_google_analytics_property_element()
-		{
-			?>
-		    	<input type="text" name="google_analytics_property_id" id="google_analytics_property_id" value="<?php echo get_option('google_analytics_property_id'); ?>" />
-		    <?php
-		}
+          function display_theme_panel_fields()
+          {
+              add_settings_section("section", "All Settings", null, "wp-seo-ga-options");
+              add_settings_section("section", "About", null, "wp-seo-ga-about");
+              add_settings_field("google_analytics_property_id", "Google Analytics Profile ID", "display_google_analytics_property_element", "wp-seo-ga-options", "section");
+              register_setting("section", "google_analytics_property_id");
+          }
 
 
-		function display_theme_panel_fields()
-		{
-			add_settings_section("section", "All Settings", null, "wp-seo-ga-options");
-			add_settings_section("section", "About", null, "wp-seo-ga-about");
-			add_settings_field("google_analytics_property_id", "Google Analytics Profile ID", "display_google_analytics_property_element", "wp-seo-ga-options", "section");
-		        register_setting("section", "google_analytics_property_id");
-		}
+          function display_google_analytics_property_element()
+          {
+              ?>
+                  <input type="text" name="google_analytics_property_id" id="google_analytics_property_id" value="<?php echo get_option('google_analytics_property_id'); ?>" />
+              <?php
+          }
+          function theme_settings_page()
+          {
+              ?>
+                  <div class="wrap">
+                  <h1>SEO Meets Google Analytics</h1>
+              <?php
+             $active_tab = isset( $_GET[ 'tab' ] ) ? $_GET[ 'tab' ] : 'general_options';
+              ?>
+                     <h2 class="nav-tab-wrapper">
+                          <a class="nav-tab <?php echo $active_tab == 'general_options' ? 'nav-tab-active' : ''; ?>" href="?page=wp-seo-ga-panel&tab=general_options" class="nav-tab">General</a>
+                          <a class="nav-tab <?php echo $active_tab == 'about' ? 'nav-tab-active' : ''; ?>" href="?page=wp-seo-ga-panel&tab=about" class="nav-tab">About</a>
+                  </h2>
+                  <form method="post" action="options.php">
+                      <?php
+                      if( $active_tab == 'general_options' ) {
+                              settings_fields("section");
 
-		add_action("admin_init", "display_theme_panel_fields");
+                          echo "<br /><br />This plugin takes care of detecting search bot visits, and register them on Google Analytics using the measurement protocol. Got to the <a href='/wp-admin/admin.php?page=wp-seo-ga-panel&tab=about'>About tab</a> to get more info.";
+                              do_settings_sections("wp-seo-ga-options");
+                              echo "* Please use a value with the following format:<br /> <strong>/(UA|YT|MO)-\d+-\d+/</strong>";
+                              submit_button();
+                      }else{
+                              do_settings_sections("wp-seo-ga-about");
+                      ?>
+                          <div id="post-body-content">
+                          WP SEO GA version 0.1
+                          <hr /><br />
+                          <br />This plugin uses <a href="https://github.com/ua-parser/uap-core">uap-core</a> to detect whenever a search bot visits your page, and then send the info about the bot and visited page to the configured Google Analytics Property ID.
+                          <br />
+                          <br />Read more about the Measurement Protocol <a href="https://developers.google.com/analytics/devguides/collection/protocol/v1/?hl=en">here</a> .
+                          <br />
+                          <br />Take in mind this a Beta product and it has not been deeply tested. Try it first on a testing enviroment before installing it on a production site.
+                          <br />
+                          <br />You may find the sourcecode on the following github repo: <a href="https://github.com/thyngster/wp-seo-ga">https://github.com/thyngster/wp-seo-ga</a>
+                          <br />You may read some more info and leave a comment on the following blog post: <a href="https://www.thyngster.com/seo-meets-ga-tracking-search-engines-visits-within-measurement-protocol">https://www.thyngster.com/seo-meets-ga-tracking-search-engines-visits-within-measurement-protocol</a>
+                          <br />
+                          <br /><a href="https://www.twitter.com/thyng">@thyng</a>
+                          &nbsp;
+                          </div>
+                         <?php
+                          }
+                          ?>
+                  </form>
+              </div>
+          <?php
+          }
 
-
-		function add_theme_menu_item()
-		{
-			add_menu_page("WP SEO GA", "WP SEO GA", "manage_options", "wp-seo-ga-panel", "theme_settings_page", null, 99);
-		}
-
-		add_action("admin_menu", "add_theme_menu_item");
-
-		function getUserIP() {
-		    if( array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER) && !empty($_SERVER['HTTP_X_FORWARDED_FOR']) ) {
-		        if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',')>0) {
-		            $addr = explode(",",$_SERVER['HTTP_X_FORWARDED_FOR']);
-		            return trim($addr[0]);
-		        } else {
-		            return $_SERVER['HTTP_X_FORWARDED_FOR'];
-		        }
-		    }
-		    else {
-		        return $_SERVER['REMOTE_ADDR'];
-		    }
-		}
-
-		function track_search_bots()
-		{
-          	     global $user_agent_parsed;
-		     if(preg_match('/(UA|YT|MO)-\\d+-\\d+/', get_option("google_analytics_property_id"), $matches)==1){
-		      $reverse = gethostbyaddr(getUserIP());
-	       	      $payload_template = array (
-		 	       'v' => '1',
-			        '_v' => 'j44',
-			        'a' => '333565997',
-	        		't' => 'pageview',
-			        '_s' => '1',
-			        'dl' => '',
-		        	'ul' => 'en',
-			        'de' => 'UTF-8',
-			        'dt' => '',
-		        	'sd' => '24-bit',
-			        'sr' => '1920x1080',
-			        'vp' => '1771x429',
-			        'je' => '0',
-			        'fl' => '22.0 r0',
-	        		'_u' => 'SDCCiEIrB~',
-			        'jid' => '',
-			        'cid' => '884755937.1468560970',
-	        		'tid' => get_option("google_analytics_property_id"),
-			        'z' => '509999257'
-		      );
-		      $cid = md5(ip2long(getUserIP()));
-		      $cid = substr_replace($cid, "-", 8, 0);
-		      $cid = substr_replace($cid, "-", 12, 0);
-		      $cid = substr_replace($cid, "-", 16, 0);
-		      $cid = substr_replace($cid, "-", 20, 0);
-
-		      $payload_template["uid"] = ip2long(getUserIP());
-		      $payload_template["cid"] = $cid;
-		      $payload_template["z"] = rand(100000,9999999);
-		      $payload_template["a"] = rand(100000,9999999);
-		      $payload_template["ds"] = "wp-seo-ga";
-		      //$payload_template["dh"] = $_SERVER["SERVER_NAME"];
-		      $payload_template["dl"] = $_SERVER["REQUEST_SCHEME"].'://'.$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
-		      $payload_template["cd1"] = $user_agent_parsed->ua->family;
-		      if(!preg_match('/.*(googlebot|google)\.com$/', $reverse, $matches) && $user_agent_parsed->ua->family == "Googlebot"){
-			      $payload_template["cd1"] = "Fake-".$user_agent_parsed->ua->family;
-		      }
-		      $payload_template["cd1"] = $user_agent_parsed->ua->family;
-		      $payload_template["cd2"] = $user_agent_parsed->device->family;
-		      $payload_template["cd3"] = $user_agent_parsed->device->model;
-		      $payload_template["cd4"] = $_SERVER['HTTP_USER_AGENT'];
-	 	      if(is_404()){
-		              $payload_template["cd5"] = "404";
-		      }else{
-		              $payload_template["cd5"] = "200";
-		      }
-
-		      $payload_template["cd6"] = $cid;
-		      $payload_template["cd7"] = ip2long(getUserIP());
-	              $payload_template["cd8"] = round(memory_get_peak_usage() / 1024 /  1024, 0);
-                      $payload_template["cd9"] = timer_stop();
-                      $payload_template["cd10"] = get_num_queries();
-       	              $payload_template["cd11"] = $user_agent_parsed->ua->family;
-                      $payload_template["cd12"] = $user_agent_parsed->device->family;
-       	              $payload_template["cd13"] = $user_agent_parsed->device->model;
-		      $hitPayload = "https://www.google-analytics.com/collect?".http_build_query($payload_template, '', '&');
-//		      $hitPayload = "https://www.thyngster.com/collect?".http_build_query($payload_template, '', '&');
-		      // Create a stream
-		      $opts = array(
-		        'http'=>array(
-       			  'method'=>"GET",
-		          'header'=>"Accept-language: en\r\n" .
-       			            "User-agent: SearchBot Tracking Plugin WP 1.0\r\n"
-		        )
-		      );
-		      $context = stream_context_create($opts);
-		      $file = file_get_contents($hitPayload, false, $context);
-		     }
-		}
         } // END public function __construct
 
+        public function trackGoogleAnalytics(){
+            $bot_details = $this->get_bot_details();
+            if($bot_details["model"]!="Spider"){
+              return;
+            }
 
+            if(preg_match('/(UA|YT|MO)-\d+-\d{1,3}$/', get_option("google_analytics_property_id"))==false){
+              return;
+            }
+
+            $core_payload_template = array (
+              'v' => '1',
+              't' => 'pageview',
+              'dl' => $_SERVER["REQUEST_SCHEME"].'://'.$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"],
+              'ul' => substr($_SERVER["HTTP_ACCEPT_LANGUAGE"],0,2),
+              'de' => 'UTF-8',
+              'dt' => '',
+              'cid' => $bot_details["ga_client_id"],
+              'uid' => $bot_details["ga_user_id"],
+              'tid' => get_option("google_analytics_property_id"),
+              'ds' => 'wp-seo-ga',
+              'a' => rand(100000,9999999),
+              'z' => rand(100000,9999999),
+              'cd1' => $bot_details["name"],
+              'cd2' => $bot_details["model"],
+              'cd3' => $bot_details["type"],
+              'cd4' => $bot_details["user_agent"],
+              'cd5' => '200',
+              'cd6' => $bot_details["ga_client_id"],
+              'cd7' => ip2long($bot_details["ip_address"]),
+              'cd8' => round(memory_get_peak_usage() / 1024 /  1024, 0),
+              'cd9' => timer_stop(),
+              'cd10' => get_num_queries(),
+              'cd11' => $bot_details["name"],
+              'cd12' => $bot_details["model"],
+              'cd13' => $bot_details["type"],
+              'cd14' => $_SERVER["REQUEST_METHOD"],
+              'cd15' => $_SERVER["SERVER_PROTOCOL"],
+              'cd16' => $bot_details["ip_reverse_domain"]
+            );
+
+            if(is_404()){
+              $core_payload_template["cd5"]= '404';
+            }
+            $this->send_ga_hit($core_payload_template);
+        }
+
+        public function send_ga_hit($payload,$endpoint = "https://www.google-analytics.com/collect"){
+          $hitPayload = "https://www.google-analytics.com/collect?".http_build_query($payload, '', '&');
+          // Create a stream
+          $opts = array(
+            'http'=>array(
+                 'method'=>"GET",
+              'header'=>"Accept-language: en\r\n" .
+                           "User-agent: SEO Meets Google Analytics Plugin WP 0.2\r\n"
+            )
+          );
+          $context = stream_context_create($opts);
+          $file = file_get_contents($hitPayload, false, $context);
+        }
+
+        public function get_bot_details(){
+          $ua = $_SERVER['HTTP_USER_AGENT'];
+          $parser = Parser::create();
+          $user_agent_parsed = $parser->parse($ua);
+          $bot_info = array(
+            'name' => $user_agent_parsed->ua->family,
+            'model' => $user_agent_parsed->device->family,
+            'type' => $user_agent_parsed->device->model,
+            'ip_address' => $this->get_ip_address(),
+            'user_agent' => $ua
+          );
+
+          $bot_info["ip_reverse_domain"] = $this->getRootDomain(gethostbyaddr($bot_info["ip_address"]));
+          // Generate UUIDv4 from Ip Address
+          $cid = md5(ip2long($bot_info["ip_address"]));
+          $cid = substr_replace($cid, "-", 8, 0);
+          $cid = substr_replace($cid, "-", 12, 0);
+          $cid = substr_replace($cid, "-", 16, 0);
+          $cid = substr_replace($cid, "-", 20, 0);
+          $bot_info["ga_client_id"] = $cid;
+
+          // Set bot's User ID
+          $bot_info["ga_user_id"] = ip2long($bot_info["ip_address"]);
+          return $bot_info;
+        }
+
+        public function getRootDomain($domain){
+          // Not the best way to do this, trying to avoid the need to deal with Top TLD lists
+          if (preg_match("/\.[a-z]{2,3}\.[a-z]{2,3}$/i", $domain)) {
+              return implode(".",array_slice(explode(".",$domain), -3, 3, true));
+          }else{
+              return implode(".",array_slice(explode(".",$domain), -2, 2, true));
+          }
+        }
+
+        public function get_ip_address() {
+          if( array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER) && !empty($_SERVER['HTTP_X_FORWARDED_FOR']) ) {
+            if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',')>0) {
+              $addr = explode(",",$_SERVER['HTTP_X_FORWARDED_FOR']);
+                return trim($addr[0]);
+              } else {
+                return $_SERVER['HTTP_X_FORWARDED_FOR'];
+              }
+            }
+            else {
+              return $_SERVER['REMOTE_ADDR'];
+            }
+        }
 
         /**
          * Activate the plugin
@@ -227,13 +233,9 @@ if(!class_exists('WP_Plugin_Seo_Ga'))
 
 if(class_exists('WP_Plugin_Seo_Ga'))
 {
-	// Installation and uninstallation hooks
-	register_activation_hook(__FILE__, array('WP_Plugin_Seo_Ga', 'activate'));
-	register_deactivation_hook(__FILE__, array('WP_Plugin_Seo_Ga', 'deactivate'));
-	// instantiate the plugin class
-	$ua = $_SERVER['HTTP_USER_AGENT'];
-//	$ua = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)';
-	$parser = Parser::create();
-	$user_agent_parsed = $parser->parse($ua);
-	$wp_plugin_seo_ga = new WP_Plugin_Seo_Ga($user_agent_parsed);
+    // Installation and uninstallation hooks
+    /*register_activation_hook(__FILE__, array('WP_Plugin_Seo_Ga', 'activate'));
+    register_deactivation_hook(__FILE__, array('WP_Plugin_Seo_Ga', 'deactivate'));
+    */
+    $wp_plugin_seo_ga = new WP_Plugin_Seo_Ga();
 }
